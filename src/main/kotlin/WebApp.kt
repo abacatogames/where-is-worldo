7c3eca2d0e8e3f67768d76f168a81bbf69533c77
cbaumont <baumont.carlos@gmail.com>
2025-11-23 18:04:56 +0000

    wip-pt-2

--- src/main/kotlin/WebApp.kt
+++ src/main/kotlin/WebApp.kt
@@ -1,31 +1,20 @@
 package io.github.cbaumont
 
 import io.github.cbaumont.view.GameView
+import io.github.cbaumont.view.generatePage
 import io.github.cbaumont.view.webView
-import io.github.cbaumont.view.generateHtml
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.html.respondHtml
-import io.ktor.server.response.respond
-import io.ktor.http.content.TextContent
-import io.ktor.server.netty.Netty
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.request.receiveParameters
+import io.ktor.http.content.TextContent
 import io.ktor.http.withCharset
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondRedirect
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
-import kotlinx.html.FormMethod
-import kotlinx.html.body
-import kotlinx.html.div
-import kotlinx.html.form
-import kotlinx.html.h1
-import kotlinx.html.h2
-import kotlinx.html.head
-import kotlinx.html.style
-import kotlinx.html.textInput
-import kotlinx.html.title
 
 fun main() {
     val previousGuesses = mutableListOf<WordGuess?>()
@@ -38,69 +27,14 @@ fun main() {
                 proposedWord = "GREENLAND",
                 gameIntro = "Where is Worldo today?"
             )
-            get("/test") {
-              val text = generateHtml(previousGuesses, game)
-              call.respond(TextContent(text, ContentType.Text.Html.withCharset(Charsets.UTF_8), HttpStatusCode.OK))
-            }
             get("/") {
-                call.respondHtml {
-                    head {
-                        title {
-                            +"Where is Worldo?"
-                        }
-                        style {
-                            +styles.toString()
-                        }
-                    }
-                    body {
-                        div("container") {
-                            h1 { +game.gameIntro }
-                            h2("instructions") {
-                                +"Worldo may be in any country in the world."
-                            }
-
-                            val validGuesses = previousGuesses.filterNotNull()
-
-                            if (validGuesses.lastOrNull()?.fullMatch == true) {
-                                h2("won") {
-                                    +"Congratulations, you found Worldo!"
-                                }
-                            } else if (validGuesses.size >= game.maxAttempts) {
-                                h2("lost") {
-                                    +"You’re out of attempts for today — better luck tomorrow!"
-                                }
-                            } else {
-                                if (previousGuesses.isNotEmpty() && previousGuesses.lastOrNull() == null) {
-                                    h2("invalid") {
-                                        +"Invalid country, please make another guess."
-                                    }
-                                } else if (previousGuesses.isEmpty()) {
-                                    h2 { +"Start by making a guess." }
-                                } else {
-                                    h2 { +"You have ${game.maxAttempts - validGuesses.size} attempts left. Make another guess." }
-                                }
-                                form(action = "/", method = FormMethod.post) {
-                                    textInput(name = "guess") {
-                                        placeholder = "TYPE HERE"
-                                        autoFocus = true
-                                    }
-                                }
-                            }
-                            div("board") {
-                                for (guess in validGuesses) {
-                                    div("row") {
-                                        guess.matches.map {
-                                            val cls = if (it.value) "correct" else "absent"
-                                            div("tile $cls") {
-                                                +guess.value[it.key].toString()
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                call.respond(
+                    TextContent(
+                        generatePage(game, previousGuesses),
+                        ContentType.Text.Html.withCharset(Charsets.UTF_8),
+                        HttpStatusCode.OK
+                    )
+                )
             }
             post("/") {
                 call.receiveParameters()["guess"]
--- src/main/kotlin/view/WebView.kt
+++ src/main/kotlin/view/WebView.kt
@@ -2,87 +2,103 @@ package io.github.cbaumont.view
 
 import io.github.cbaumont.GameLoop
 import io.github.cbaumont.WordGuess
+import io.github.cbaumont.styles
 import io.ktor.server.request.receiveParameters
 import io.ktor.server.routing.RoutingCall
 import kotlinx.coroutines.runBlocking
+import kotlinx.html.FlowContent
 import kotlinx.html.FormMethod
+import kotlinx.html.HTML
+import kotlinx.html.body
 import kotlinx.html.div
 import kotlinx.html.form
+import kotlinx.html.h1
 import kotlinx.html.h2
 import kotlinx.html.head
-import kotlinx.html.h1
+import kotlinx.html.html
 import kotlinx.html.stream.createHTML
 import kotlinx.html.style
 import kotlinx.html.textInput
 import kotlinx.html.title
-import kotlinx.html.body
-import kotlinx.html.div
-import io.github.cbaumont.styles
-
 
 fun GameView.Companion.webView(call: RoutingCall? = null): GameView =
-        object : GameView {
-          override fun displayGuess(guess: WordGuess): String {
+    object : GameView {
+        override fun displayGuess(guess: WordGuess): String {
             val div =
-                    createHTML().div("row") {
-                      guess.matches.map {
+                createHTML().div("row") {
+                    guess.matches.map {
                         val cls = if (it.value) "correct" else "absent"
                         div("tile $cls") { +guess.value[it.key].toString() }
-                      }
                     }
+                }
             return div
-          }
+        }
 
-          override fun readInput(): String {
+        override fun readInput(): String {
             return runBlocking { call?.receiveParameters()?.get("guess") }?.trim() ?: ""
-          }
-
-          override fun displayMessage(message: String) {}
         }
 
-fun headHtml(style: String): String =
-        createHTML().head {
-          title { +"Where is Worldo?" }
-          style { +style }
-        }
+        override fun displayMessage(message: String) {}
+    }
 
-fun gameInstructions(previousGuesses: List<WordGuess?>, game: GameLoop): String {
-  val validGuesses = previousGuesses.filterNotNull()
-  var res =
-          if (validGuesses.lastOrNull()?.fullMatch == true) {
-            createHTML().h2("won") { +"Congratulations, you found Worldo!" }
-          } else if (validGuesses.size >= game.maxAttempts) {
-            createHTML().h2("lost") { +"You’re out of attempts for today — better luck tomorrow!" }
-          } else {
-            if (previousGuesses.isNotEmpty() && previousGuesses.lastOrNull() == null) {
-              createHTML().h2("invalid") { +"Invalid country, please make another guess." }
-            } else if (previousGuesses.isEmpty()) {
-              createHTML().h2 { +"Start by making a guess." }
-            } else {
-              createHTML().h2 {
+private fun HTML.titleAndStyle(): Unit =
+    head {
+        title { +"Where is Worldo?" }
+        style { +styles.toString() }
+    }
+
+private fun FlowContent.guessForm() =
+    form(action = "/", method = FormMethod.post) {
+        textInput(name = "guess") {
+            placeholder = "TYPE HERE"
+            autoFocus = true
+        }
+    }
+
+private fun FlowContent.gameInstructions(previousGuesses: List<WordGuess?>, game: GameLoop) {
+    val validGuesses = previousGuesses.filterNotNull()
+    return if (validGuesses.lastOrNull()?.fullMatch == true) {
+        h2("won") { +"Congratulations, you found Worldo!" }
+    } else if (validGuesses.size >= game.maxAttempts) {
+        h2("lost") { +"You’re out of attempts for today — better luck tomorrow!" }
+    } else {
+        if (previousGuesses.isNotEmpty() && previousGuesses.lastOrNull() == null) {
+            h2("invalid") { +"Invalid country, please make another guess." }
+        } else if (previousGuesses.isEmpty()) {
+            h2 { +"Start by making a guess." }
+        } else {
+            h2 {
                 +"You have ${game.maxAttempts - validGuesses.size} attempts left. Make another guess."
-              }
             }
-          }
-
-  val input =
-          createHTML().form(action = "/", method = FormMethod.post) {
-            textInput(name = "guess") {
-              placeholder = "TYPE HERE"
-              autoFocus = true
-            }
-          }
-
-  return res + input
+        }
+        guessForm()
+    }
 }
 
-fun generateHtml(previousGuesses: List<WordGuess?>, game: GameLoop): String {
-  
-  val body = createHTML().body { div("container") {
-    h1 { +game.gameIntro }
-    h2("instructions") { +"Worldo may be in any country in the world."}
-    +gameInstructions(previousGuesses, game)
-  } }
-  return headHtml(styles.toString()) + body
-}
+private fun FlowContent.gameBoard(validGuesses: List<WordGuess>) =
+    div("board") {
+        for (guess in validGuesses) {
+            div("row") {
+                guess.matches.map {
+                    val cls = if (it.value) "correct" else "absent"
+                    div("tile $cls") {
+                        +guess.value[it.key].toString()
+                    }
+                }
+            }
+        }
+    }
+
+fun generatePage(game: GameLoop, previousGuesses: List<WordGuess?>): String =
+    createHTML().html {
+        titleAndStyle()
+        body {
+            div("container") {
+                h1 { +game.gameIntro }
+                h2 { +"Worldo may be in any country in the world." }
+                gameInstructions(previousGuesses, game)
+                gameBoard(previousGuesses.filterNotNull())
+            }
+        }
+    }
 
