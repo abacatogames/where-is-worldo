ee3cc2f3fad5aaa51c7c3c53c8760c481741b44c
cbaumont <baumont.carlos@gmail.com>
2026-02-23 18:31:33 +0000

    Determine cardinal directions

--- src/main/kotlin/geo/GeoDistance.kt
+++ src/main/kotlin/geo/GeoDistance.kt
@@ -1,16 +1,39 @@
 package io.github.cbaumont.geo
 
+import kotlin.math.roundToInt
+import net.sf.geographiclib.Geodesic
+
 fun interface GeoDistance : (GeoLocation, GeoLocation) -> Distance {
     companion object {
         fun create(): GeoDistance =
-            object : GeoDistance {
-                override fun invoke(p1: GeoLocation, p2: GeoLocation): Distance {
-                    TODO("Not yet implemented")
-                }
+            GeoDistance { firstLocation, secondLocation ->
+                val geodesicData =
+                    Geodesic.WGS84.Inverse(
+                        firstLocation.latitude,
+                        firstLocation.longitude,
+                        secondLocation.latitude,
+                        secondLocation.longitude
+                    )
+
+                val direction = determineDirection(geodesicData.azi1)
+
+                Distance((geodesicData.s12 / 1000.00).roundToInt(), direction)
             }
     }
 }
 
-data class Distance(val km: Double, val direction: CardinalDirection)
+private fun determineDirection(azimuth: Double): CardinalDirection =
+    when {
+        azimuth in -10.0..<10.0 -> CardinalDirection.NORTH
+        azimuth in 10.0..<67.5 -> CardinalDirection.NORTH_EAST
+        azimuth in 67.5..<112.5 -> CardinalDirection.EAST
+        azimuth in 112.5..<170.0 -> CardinalDirection.SOUTH_EAST
+        azimuth >= 170.0 || azimuth < -170.0 -> CardinalDirection.SOUTH
+        azimuth in -170.0..<-112.5 -> CardinalDirection.SOUTH_WEST
+        azimuth in -112.5..<-67.5 -> CardinalDirection.WEST
+        else -> CardinalDirection.NORTH_WEST
+    }
+
+data class Distance(val km: Int, val direction: CardinalDirection)
 
 enum class CardinalDirection { SOUTH, WEST, NORTH, EAST, SOUTH_WEST, SOUTH_EAST, NORTH_WEST, NORTH_EAST }
--- src/test/kotlin/GeoDistanceTest.kt
+++ src/test/kotlin/GeoDistanceTest.kt
@@ -1,20 +1,105 @@
+import io.github.cbaumont.geo.CardinalDirection
 import io.github.cbaumont.geo.CountriesTable
-import net.sf.geographiclib.Geodesic
+import io.github.cbaumont.geo.Distance
+import io.github.cbaumont.geo.GeoDistance
+import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
 
 class GeoDistanceTest {
 
     @Test
-    fun `calculates the distance between two countries`() {
-
-        val country2 = CountriesTable.find { it.name == "Argentina" }!!
+    fun `calculates the distance between two countries - SW`() {
         val country1 = CountriesTable.find { it.name == "Brazil" }!!
+        val country2 = CountriesTable.find { it.name == "Argentina" }!!
 
+        val result = GeoDistance.create()(country1, country2)
 
-        val distance =
-            Geodesic.WGS84.Inverse(country1.latitude, country1.longitude, country2.latitude, country2.longitude)
+        val expected = Distance(2916, CardinalDirection.SOUTH_WEST)
 
-        println(distance.s12)
-        println(distance.azi1)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - S`() {
+        val country1 = CountriesTable.find { it.name == "Niger" }!!
+        val country2 = CountriesTable.find { it.name == "Nigeria" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(945, CardinalDirection.SOUTH)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - N`() {
+        val country1 = CountriesTable.find { it.name == "Nigeria" }!!
+        val country2 = CountriesTable.find { it.name == "Niger" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(945, CardinalDirection.NORTH)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - E`() {
+        val country1 = CountriesTable.find { it.name == "Algeria" }!!
+        val country2 = CountriesTable.find { it.name == "Libya" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(1553, CardinalDirection.EAST)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - W`() {
+        val country1 = CountriesTable.find { it.name == "Togo" }!!
+        val country2 = CountriesTable.find { it.name == "Ghana" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(217, CardinalDirection.WEST)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - NW`() {
+        val country1 = CountriesTable.find { it.name == "Algeria" }!!
+        val country2 = CountriesTable.find { it.name == "Morocco" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(942, CardinalDirection.NORTH_WEST)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - NE`() {
+        val country1 = CountriesTable.find { it.name == "Chad" }!!
+        val country2 = CountriesTable.find { it.name == "Egypt" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(1774, CardinalDirection.NORTH_EAST)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `calculates the distance between two countries - SE`() {
+        val country1 = CountriesTable.find { it.name == "Bolivia" }!!
+        val country2 = CountriesTable.find { it.name == "Paraguay" }!!
+
+        val result = GeoDistance.create()(country1, country2)
+
+        val expected = Distance(958, CardinalDirection.SOUTH_EAST)
+
+        assertEquals(expected, result)
     }
 }
